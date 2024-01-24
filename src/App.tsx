0d1c9ac288260a568afab83688f1057c052d86c8
Kidongg <dlrlehd7@daum.net>
2024-01-24 11:43:27 +0900

    set layout & add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,57 @@
-function App() {
+import Home from "./pages/home/Home";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Menu from "./components/menu/Menu";
+import Login from "./pages/login/Login";
+import "./styles/global.scss";
 
-  return (
-    <div>Hello World</div>
-  )
+function App() {
+  //* 레이아웃
+  const Layout = () => {
+    return (
+      <div className="main">
+        <Navbar />
+        <div className="container">
+          <div className="menuContainer">
+            <Menu />
+          </div>
+          <div className="contentContainer">
+            {/* 동적인 컴포넌트 */}
+            <Outlet />
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  };
+
+  //* 라우팅
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/users",
+          element: <Users />,
+        },
+        {
+          path: "/products",
+          element: <Products />,
+        },
+      ],
+    },
+    { path: "/login", element: <Login /> },
+  ]);
+
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,32 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src="/logo.svg" alt="" />
+        <span>Invigoworks Admin</span>
+      </div>
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" className="icon" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/blog-a27f7.appspot.com/o/images%2Fposts%2Fnextjs-conf%2Fnextjs.png?alt=media&token=09247773-9707-4dd1-b3ca-3fe7f943497a"
+            alt=""
+            className="icon"
+          />
+          <span>Kidongg</span>
+        </div>
+        <img src="/setting.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
